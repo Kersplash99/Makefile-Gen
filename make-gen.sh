e0e539a8d9a3954086468285cc7f1566f06f2a17
kersplash99 <ammarabbas99@gmail.com>
2017-12-19 20:36:09 -0500

    Fixed indentations.
    - I was using hard tabs, GASP, changed them...

--- make-gen.sh
+++ make-gen.sh
@@ -5,28 +5,28 @@ type=$1
 name=$2
 
 if [ -n "$type" -a -n "$name" -a "$type" == "Makefile" ]; then
-	# create the MakeFile
-	touch Makefile
+  # create the MakeFile
+  touch Makefile
 
-	# add in compiler information
-	echo "# compiler to use" > MakeFile
-	echo "CC = gcc" >> MakeFile
-	echo "" >> MakeFile
-	
-	# add in compiler flags and options
-	echo "# compiler flags (c99 standard)" >> MakeFile
-	echo "CFLAGS = -Wall -std=c99" >> MakeFile
-	echo "" >> MakeFile
+  # add in compiler information
+  echo "# compiler to use" > MakeFile
+  echo "CC = gcc" >> MakeFile
+  echo "" >> MakeFile
+  
+  # add in compiler flags and options
+  echo "# compiler flags (c99 standard)" >> MakeFile
+  echo "CFLAGS = -Wall -std=c99" >> MakeFile
+  echo "" >> MakeFile
 
-	# add the specified target
-	if [ -n "$name" ]; then	
-		echo "# target files" >> MakeFile
-		echo "$name: $name.o" >> MakeFile
-	fi
+  # add the specified target
+  if [ -n "$name" ]; then 
+    echo "# target files" >> MakeFile
+    echo "$name: $name.o" >> MakeFile
+  fi
 
 else
-	echo "Syntax: gen <type> <name>"
-	echo "Valid Types: Makefile"
+  echo "Syntax: gen <type> <name>"
+  echo "Valid Types: Makefile"
 fi
 
 
