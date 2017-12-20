d67fa24814e1053517e480017a0cab9b32940ec0
kersplash99 <ammarabbas99@gmail.com>
2017-12-19 23:08:35 -0500

    Added variables to make customization easier

--- make-gen.sh
+++ make-gen.sh
@@ -4,22 +4,27 @@
 # create the MakeFile
 touch Makefile
 
+# create and set default variables
+compiler="gcc"
+flags="-Wall -std=c99"
+
+
 # add in compiler information
 echo "# compiler to use" > MakeFile
-echo "CC = gcc" >> MakeFile
+echo "CC = ${compiler}" >> MakeFile
 echo "" >> MakeFile
 
 # add in compiler flags and options
 echo "# compiler flags (c99 standard)" >> MakeFile
-echo "CFLAGS = -Wall -std=c99" >> MakeFile
+echo "CFLAGS = ${flags}" >> MakeFile
 echo "" >> MakeFile
 
 # are there enough options
 if [ "$#" -ge "1"  ]; then
   # target file name (executable file)
-  target = $1;
+  target="$1";
   if [ -n "$target" ]; then
     echo "# target files" >> MakeFile
-    echo "$target: $target.o" >> MakeFile
+    echo "${target}: ${target}.o" >> MakeFile
   fi
 fi
