f1b30d4863b57cd6c8761607e0ccf913112347c0
kersplash99 <ammarabbas99@gmail.com>
2017-12-19 22:47:22 -0500

    Changed stuff
    - added all the functionality it had but made it easier to update
    - cleared out some things
    - will now generate a basic make file even without a target file specified

--- make-gen.sh
+++ make-gen.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 # created by: Ammar Abbas
 
+# create the MakeFile
+touch Makefile
 
+# add in compiler information
+echo "# compiler to use" > MakeFile
+echo "CC = gcc" >> MakeFile
+echo "" >> MakeFile
 
+# add in compiler flags and options
+echo "# compiler flags (c99 standard)" >> MakeFile
+echo "CFLAGS = -Wall -std=c99" >> MakeFile
+echo "" >> MakeFile
+
+# are there enough options
+if [ "$#" -lt "2"  ]; then
+  echo "Incorrect options."
+  echo "Syntax: make-gen.sh <target name> ..."
+else
+  # target file name (executable file)
+  target = $1;
+  if [ -n "$target" ]; then
+    echo "# target files" >> MakeFile
+    echo "$target: $target.o" >> MakeFile
+  fi
+fi
